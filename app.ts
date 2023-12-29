a5d618d88af682219bb2ad5700d388c3cca12db6
Nad <anwarnadeem2010@gmail.com>
2023-12-29 22:15:55 +0530

    types end

--- app.ts
+++ app.ts
@@ -1,15 +1,49 @@
 console.log('Time to get started...');
 
 
-function add(num1:number,num2:number){
-    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
-        throw new Error("Incorrect input"); 
+//javascript is dynamic type but typescript is static type
+
+function add(num1:number,num2:number,showRes:boolean,phrase:string){
+
+
+    // if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+    //     throw new Error("Incorrect input"); 
+    // }
+
+    //types of data types in typescript
+    // 1.number
+    // 2.boolean
+    // 3.string
+
+    let result = num1+num2;
+    if(showRes){
+        console.log(phrase+result);
+    }else{
+
+        return num1+num2;
     }
-    return num1+num2;
 }
 
-let a = "10";
+//In javascript Number and String is diff in typescript number and string
+
+let a = 10;
 let b = 20;
 
-const res = add(a,b);
-console.log(res);
+const printres = true;
+const phrase = "The result is ";
+
+
+let data:number;
+data=30;
+
+let price:number;
+price="20";
+
+
+
+
+// const res = add(a,b,printres);
+// console.log(res);
+
+add(a,b,printres,phrase)
+
