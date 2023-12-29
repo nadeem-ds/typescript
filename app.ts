5bdebcbc37867f9d8d5bad7bc6991a5de73ed759
Nad <anwarnadeem2010@gmail.com>
2023-12-29 21:55:09 +0530

    how can we do in js

--- app.ts
+++ app.ts
@@ -1,11 +1,14 @@
 console.log('Time to get started...');
 
 
-function add(num1,num2){
+function add(num1:number,num2:number){
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        throw new Error("Incorrect input"); 
+    }
     return num1+num2;
 }
 
-let a="10";
+let a = "10";
 let b = 20;
 
 const res = add(a,b);
